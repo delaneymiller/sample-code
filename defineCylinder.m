3f19197939d52fa5caad618f5878ad5c7a623e2b
delaneymiller <delaneymiller@users.noreply.github.com>
2017-03-27 09:07:11 -0400

    Add files via upload

---
+++ defineCylinder.m
@@ -0,0 +1,18 @@
+function [x_coords y_coords] = defineCylinder(n,m,x0,y0,radius)
+% Finds the x and y coordinates of all of the grid points in the cylinder
+% and outputs two arrays: one for the x-coordinates (m) and one for the
+% y-coordinates (n).
+x_coords = [];
+y_coords = [];
+for b = 1:n
+    for a = 1:m
+        dist = sqrt((a-x0)^2 + (b-y0)^2);
+        if dist <= radius
+            x_coords = cat(1,x_coords,a);
+            y_coords = cat(1,y_coords,b);
+        end
+    end
+end
+
+end
+
